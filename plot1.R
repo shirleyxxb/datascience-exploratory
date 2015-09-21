146912a16483ff619361bdd31bc56955594fa2c2
jpeterson <jonathan.peterson@damballa.com>
2015-09-21 12:03:35 -0500

    First plot

---
+++ plot1.R
@@ -0,0 +1,30 @@
+# plot1.R: Creates a graph of total PM emissions by year
+library(dplyr)
+
+# Load and clean incoming data
+load_and_clean_dataset <- function(filename) {
+  nei <- readRDS("summarySCC_PM25.rds")
+  nei %>% group_by(year) %>% summarize(total = sum(Emissions))
+}
+
+# Crete an appropriately themed plot
+make_plot <- function(dataset) {
+  options(scipen = 5)  # Don't use scientific notation
+  plot(dataset$year, dataset$total,
+       pch=15,
+       col="blue",
+       main="PM2.5 Emissions by Year per National Emissions Inventory",
+       xlab="Year",
+       xaxt='n',
+       ylab="Total Emissions in Tons")
+  axis(side=1, at=dataset$year, labels=T)
+  # Add a linear fit line
+  model <- lm(dataset$total ~ dataset$year)
+  abline(model)
+}
+
+# Create the requested file
+png("plot1.png")
+data <- load_and_clean_dataset("summarySCC_PM25.rds")
+make_plot(data)
+dev.off()
